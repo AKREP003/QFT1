3a48372ec866fdb040f042ab342889eafeeef718
AKREP003 <aliekremkuran@gmail.com>
2025-08-08 20:45:15 +0300

    aa

--- src/Scene.java
+++ src/Scene.java
@@ -46,7 +46,31 @@ public class Scene {
         }
     }
 
-    double density(Fermion f) {return 0;}
+    double density(Fermion f) {
+
+        ComplexNumber[] buffer = new ComplexNumber[4];
+
+        for (int i = 0; i < 4; i++) {
+
+            buffer[i] = f.components[i].copy();
+
+        }
+
+        evolveSpin(this.timePerStep, f);
+
+        for (int i = 0; i < 4; i++) {
+
+            buffer[i].subtract(f.components[i]);
+
+            buffer[i].multiply(new ComplexNumber(-1, 0));
+
+        }
+
+        return f.getDiracDensity(buffer, new double[]{0,0,0,0}, this.timePerStep);
+
+
+
+    }
 
     ComplexNumber feynman(double S) {return GammaMatrices.exp(new ComplexNumber(S, 0));}
 
